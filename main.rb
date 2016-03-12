fbe4e7b8c136ba25dc0ac0e45bc64089c05de02b
RusProgrammer <ilkaboomboom@rambler.ru>
2016-03-12 14:51:40 +0300

    15.51

--- main.rb
+++ main.rb
@@ -1,6 +1,15 @@
-﻿
-puts "hello world"
-puts "good-bye world"
-^Z
-hello world
-good-bye world
+﻿class Car
+    @@NUM_CARS = 0
+    def initialize
+        @@NUM_CARS = @@NUM_CARS +  1
+        puts @@NUM_CARS
+    end
+end
+class SportsCar < Car
+end
+class FamilyCar < Car
+end
+
+a = Car.new
+b = SportsCar.new
+c = FamilyCar.new
